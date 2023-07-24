ec1ff47a759d49d562f5dd67481e5053223849af
Preet Lakra <lakraaman07@gmail.com>
2023-07-24 23:43:14 +0530

    Build Done

--- weather-app/script.js
+++ weather-app/script.js
@@ -2,25 +2,27 @@ const button = document.getElementById("button");
 
 const input = document.getElementById("city-input");
 
-const cityName = document.getElementById('city-name');
+const cityName = document.getElementById("city-name");
 
-const cityTime = document.getElementById('city-time');
+const cityTime = document.getElementById("city-time");
 
-const cityTemp = document.getElementById('city-temp');
+const cityTemp = document.getElementById("city-temp");
 
 
 async function getData(cityName){
     const promise  = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=42827f84082245faae1180221232307&q=${cityName}&aqi=yes` 
     );
-    return await promise.json()
+    return await promise.json();
 }
 
-button.addEventListener('click', async() =>{
+button.addEventListener("click", async () => {
     const value = input.value;
     const result  = await getData(value);
-    cityName.innerText = `${result.location.name}, ${result.location.name}`, `${result.location.region}`, `${result.location.country}`
-
+    console.log(result);
+    cityName.innerText = `${result.location.region}`;
+    cityTime.innerText = `${result.location.localtime}`;
+    cityTemp.innerText = `${result.current.temp_c}`;
 });
 
 // http://api.weatherapi.com/v1/current.json?key=42827f84082245faae1180221232307&q=London&aqi=yes
